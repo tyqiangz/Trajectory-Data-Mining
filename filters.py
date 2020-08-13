0eac0b3cec001d07e0ef43274a2bed4158d42085
tyqiangz <tay.yongqiang@gmail.com>
2020-08-13 17:24:39 +0800

    Implemented 1 dimensional first order Kalman filter

--- filters.py
+++ filters.py
@@ -1,8 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from numpy import dot, zeros, eye, isscalar, shape
-from math import log, exp, sqrt
-import numpy.linalg as linalg
+import math
 
 class KalmanFilter:
     '''
@@ -20,59 +18,100 @@ class KalmanFilter:
 
     assumptions: we consider the Kalman filter with constant acceleration and non-constant velocity
     '''
-    def __init__(self, H, x_0, P_0, R, data):
-        if dim_x < 1:
-            raise ValueError('dim_x must be 1 or greater')
-        if dim_z < 1:
-            raise ValueError('dim_z must be 1 or greater')
-        
+    def __init__(self, z, H, Q, R, x_est, P_est):
+        self.z = z
         self.H = H
-        self.P_0 = P_0
-        self.data = data
-
-        self.x = x_0
-        self.z = np.array([[None]*self.dim_z]).T
-
-
+        self.Q = Q
+        self.R = R
+        self.x_est = x_est
+        self.P_est = P_est
     
-    def genNextStep():
-        pass
+    def kalmanGain(self, P, H, R):
+        K = R + np.dot(np.dot(H, P), H.T)
 
-def genTrajectory(NUM_POINTS = 20):
-    # randomly generate distance to take for each time step
-    x_points = np.array([random.randrange(start=-50, stop=100) for i in range(NUM_POINTS)])/100
-    y_points = np.array([random.randrange(start=-50, stop=100) for i in range(NUM_POINTS)])/100
-    t_points = np.array([random.randrange(start=0, stop=1000) for i in range(NUM_POINTS)])/100
-    rad_inacc = np.array([random.randrange(start=0, stop=100) for i in range(NUM_POINTS)])/100
+        if type(K) != np.ndarray:
+            return np.dot(P, H.T) * (1/K)
+        else:
+            K = np.dot(np.dot(P, H.T), np.linalg.inv(K))
+            return K
+            
+    def update(self, P, H, R, z, x):
+        '''
+        :param P: state error covariance matrix
+        :param H: output transition matrix
+        :param R: measurement noise covariance matrix
+        :param z: new measurement
+        :param x: old state estimate
+        :return x_new: new state estimate
+        :return P: new state error covariance matrix
+        '''
+
+        K = kalmanGain(P=P, H=H, R=R)
+        x_new = x + np.dot(K, z - np.dot(H, x))
+        P_new = np.dot(np.eye(len(P)) - np.dot(K,H), P)
+
+        return x_new, P_new
+
+    def predict(self, Phi, x, P, Q):
+        '''
+        :param Phi: state error covariance
+        :param x: current state
+        :param P: current state error covariance matrix
+        :param Q: process noise covariance matrix
+        :return x_pred: prediction of the next state
+        :return P_pred: prediction of the next state error covariance matrix
+        '''
+        x_pred = np.dot(Phi, x)
+        P_pred = np.dot(np.dot(Phi,P), Phi.T) + Q
+
+        return x_pred, P_pred
     
-    # trajectory is just a 3D array of latitude, longitude, time
-    traj = np.zeros(shape=(NUM_POINTS+1,4))
-
-    for i in range(1, NUM_POINTS+1):
-        traj[i,0] = traj[i-1,0] + x_points[i-1]
-        traj[i,1] = traj[i-1,1] + y_points[i-1]
-        traj[i,2] = traj[i-1,2] + t_points[i-1]
+    def predictAll(self):
+        x_est = self.x_init
+        P_est = self.P_init
         
-    traj[0,3] = random.randrange(start=0,stop=100)/100
-    traj[1:,3] = rad_inacc
-    
-    traj = pd.DataFrame(traj)
-    traj.columns = ["lon", "lat", "time", "inacc_radius"]
-    
-    return traj
-        
-if "__name__" == "__main__":
-    # test if the velocity model works
-    sigma_sq = 2.5
-    
-    H = np.array([[1,0,0,0],[0,1,0,0]])
-    R = sigma_sq * eye(2)
-    
-    data = genTrajectory()
-    x_0 = np.array([data.lon[0], data.lat[0]])
-    
-    kf = KalmanFilter(H, x_0, P_0, R, data)
+        for i in range(1, len(self.t)):
+            Phi_i = np.array([[1, self.t[i]-self.t[i-1]], [0, 1]])
+            x_pred, P_pred = predict(Phi_i, x_est, P_est, Q)
+            x_est, P_est = update(P_pred, H, R, z[i], x_pred)
 
+            X_est = np.concatenate((X_est, np.array([x_est])))
+        
+        return X_est
+
+def kalmanWalk(NUM_POINTS=50, velocity=2, sigma2_R=0.1, sigma2_Q=0.0001):
+    '''
+    generates a constant velocity walk following the measurement model and dynamic model
+    '''
+    H = np.array([1,0])
+    x_0 = np.array([0,velocity])
+    Q = np.array([[0,0], [0,sigma2_Q]]) # process noise
+    R = sigma2_R # measurement noise
+    
+    # initialisation
+    x = np.array([x_0])
+    z = np.array([np.random.normal(loc=np.dot(H, x_0), scale=math.sqrt(sigma2_R))])
+    t = [0]
+    
+    for i in range(NUM_POINTS-1):
+        delta_t = np.random.uniform(0, 10)
+        t.append(t[-1] + delta_t)
+        
+        Phi_1 = np.array([[1, delta_t],[0, 1]])
+        x_i = np.random.multivariate_normal(mean=np.dot(Phi_1, x[i-1]), cov=Q)
+        x_i = np.array([x_i])
+        x = np.concatenate((x, x_i))
+
+        z_i = np.random.normal(loc=np.dot(H, x[i-1]), scale=math.sqrt(R))
+        z_i = np.array([z_i])
+        z = np.concatenate((z, z_i))
+    
+    return x, z, t
+    
+    
+
+#######################################################################
+# filters that I will implement when I have the time
 #######################################################################
 
 class BayesianFilter:
